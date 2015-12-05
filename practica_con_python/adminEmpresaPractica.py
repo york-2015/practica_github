b89d322f62c5beec39583ec0501baf5f7a009050
york-2015 <york-17@hotmail.com>
2015-12-05 16:14:25 -0300

    todo okey

--- practica_con_python/adminEmpresaPractica.py
+++ practica_con_python/adminEmpresaPractica.py
@@ -12,4 +12,33 @@ gastosAdministrativo = 0
 
 gastosComercial = 0 
 
-gastoFinanciero o fiscal
+gastoFinanciero = 0
+
+sumaGastos = gastoFinanciero + gastosComercial + gastosAdministrativos
+
+perdidaNeta = sumaGastos - ganaciaBruta
+
+#activos 
+activoDeUso = 0  
+activoDeDisponibilidad = 0
+activoDeCredito = 0
+activoSuma = activoDeDisponibilidad + activoDeCredito + activoDeUso
+#pasivo
+pasivoComercial = 0
+pasivoFiscal = 0
+pasivoFinanciero = 0
+pasivoSuma = pasivoFinanciero + pasivoComercial + pasivoFiscal
+#patrimonio neto 
+patrimonioNetoSuma = 0
+
+##############Resultado del ejercicio##############################3
+#A la suma del patrimonio se le resta la perdida neta 
+patrimonioNetoSuma -= perdidaNeta
+
+####Ecuacion patrimonial########
+
+if activoSuma == pasivoSuma + patrimonioNetoSuma :
+	print 'la ecuacino a dado %f', activoSuma 
+
+ 
+ 
